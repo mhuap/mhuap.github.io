ec4e0e703954ec5b2114fac2695cad4be348fd39
mhuap <huapmar97@gmail.com>
2024-05-06 12:09:59 -0500

    Updates

--- component---src-pages-404-js-e46d666adddca5d1df73.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmatias_huapaya_portfolio=self.webpackChunkmatias_huapaya_portfolio||[]).push([[883],{429:function(e,t,a){a.r(t);var l=a(7294),n=a(262);t.default=function(){return l.createElement("div",{className:"full-for-center"},l.createElement("div",{className:"center"},l.createElement(n.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not found"),l.createElement("p",null,"This page does not exist... oops.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e46d666adddca5d1df73.js.map
